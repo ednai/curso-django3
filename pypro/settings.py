d1097cae3af06ead1f7da7d7ced265628a62cbea
ednai <47866756+ednai@users.noreply.github.com>
2022-10-25 20:28:44 -0300

    adicionanando allowed host

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,10 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
+from functools import partial
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import  dj_database_url
+from decouple import config, Csv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kzx*8&u)dy#(p^lw8uwel62_2=v1e-n$wrxwg$_maq&dsit9zf'
-
+#SECRET_KEY = 'django-insecure-kzx*8&u)dy#(p^lw8uwel62_2=v1e-n$wrxwg$_maq&dsit9zf'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True,cast=bool)
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',  cast=Csv())
 
 
 # Application definition
@@ -72,13 +77,14 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 
 
 # Database
+
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+default_db_url='sqlite:///'+os.path.join(BASE_DIR,'db.sqlite3')
+parse_database=partial(dj_database_url.parse,conn_max_age=600)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':config('DATABASE_URL',default=default_db_url, cast=parse_database)
+
 }
 
 
